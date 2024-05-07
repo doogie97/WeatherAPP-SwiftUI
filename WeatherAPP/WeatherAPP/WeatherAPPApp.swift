04f34bbf241c6862dc744544d47166cfc71afe09
Doogie <chris_97@naver.com>
2024-05-07 22:51:52 +0900

    chore: 프로젝트 생성

---
+++ WeatherAPP/WeatherAPP/WeatherAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAPPApp.swift
+//  WeatherAPP
+//
+//  Created by Doogie on 5/7/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
