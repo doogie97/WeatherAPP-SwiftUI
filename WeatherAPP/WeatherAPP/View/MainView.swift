50885ab9b6f06323171731e32b16f1bbc7e68eff
Doogie <chris_97@naver.com>
2024-05-07 23:09:06 +0900

    chore: 프로젝트 초기 구성

---
+++ WeatherAPP/WeatherAPP/View/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  WeatherAPP
+//
+//  Created by Doogie on 5/7/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
