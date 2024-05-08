a58758f5220ef8e878c67e38ddb79ee279689e63
Doogie <chris_97@naver.com>
2024-05-08 23:52:08 +0900

    chore: 위젯 파일 생성

---
+++ WeatherAPP/WeatherAPPWidget/WeatherAPPWidget.swift
@@ -0,0 +1,49 @@
+//
+//  WeatherAPPWidget.swift
+//  WeatherAPPWidget
+//
+//  Created by Doogie on 5/8/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+    let emoji: String
+}
+
+struct WeatherAPPWidgetEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        VStack {
+            Text("Hi:")
+            Text(entry.date, style: .time)
+
+            Text("Emoji:")
+            Text(entry.emoji)
+        }
+    }
+}
+
+struct WeatherAPPWidget: Widget {
+    let kind: String = "WeatherAPPWidget"
+    
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            WeatherAPPWidgetEntryView(entry: entry)
+                .padding()
+                .background()
+        }
+        .configurationDisplayName("My Widget")
+        .description("This is an example widget.")
+    }
+}
+
+#Preview(as: .systemSmall) {
+    WeatherAPPWidget()
+} timeline: {
+    SimpleEntry(date: .now, emoji: "😀")
+    SimpleEntry(date: .now, emoji: "🤩")
+}
---
+++ WeatherAPP/WeatherAPPWidget/WeatherAPPWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherAPPWidgetBundle.swift
+//  WeatherAPPWidget
+//
+//  Created by Doogie on 5/8/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WeatherAPPWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        WeatherAPPWidget()
+    }
+}
