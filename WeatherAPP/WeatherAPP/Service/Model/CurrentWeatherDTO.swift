e089a8b0ffb2ade9b2177609f10f803e490119d6
Doogie <chris_97@naver.com>
2024-05-08 13:03:36 +0900

    feat: 최근 날씨 DTO생성 및 entity변환 기능 구형

---
+++ WeatherAPP/WeatherAPP/Service/Model/CurrentWeatherDTO.swift
@@ -0,0 +1,31 @@
+//
+//  CurrentWeatherDTO.swift
+//  WeatherAPP
+//
+//  Created by Doogie on 5/8/24.
+//
+
+import Foundation
+
+struct CurrentWeatherDTO: Decodable {
+    let date: Double?
+    let weather: [Weather]?
+    let main: Main?
+    let sys: Sys?
+    
+    struct Weather: Decodable {
+        let description: String?
+        let icon: String?
+    }
+    
+    struct Main: Decodable {
+        let temp: Double?
+        let temp_min: Double?
+        let temp_max: Double?
+    }
+    
+    struct Sys: Decodable {
+        let sunrise: Double?
+        let sunset: Double?
+    }
+}
