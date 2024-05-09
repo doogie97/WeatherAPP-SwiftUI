0c0f8d2868c21ca3bdd95feffd9a23d28d648f7e
Doogie <chris_97@naver.com>
2024-05-09 13:03:00 +0900

    feat: WidgetData를 Json으로writh, read 기능 구현

--- WeatherAPP/WeatherAPP/Service/Model/WidgetData.swift
+++ WeatherAPP/WeatherAPP/Service/Model/WidgetData.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct WidgetData {
+struct WidgetData: Codable {
     let date: Date
     let location: String
     let temperature: String
@@ -25,4 +25,57 @@ extension WidgetData {
                                     maxTemperature: 24.temperatureString,
                                     icon: "sun.max.fill",
                                     weather: "겁나 맑음")
+  
+  static let dataURL = FileManager.sharedContainer?.appendingPathComponent("widgetData.json")
+  
+  static func wirte(location: String?, currentWeather: CurrentWeather?, forecast: [Forecast]?) {
+    guard let location = location,
+          let currentWeather = currentWeather,
+          let forecast = forecast,
+          let dataURL = dataURL else {
+      return
+    }
+    
+    var list = [WidgetData]()
+    let data = WidgetData(date: currentWeather.forecastedDate,
+                          location: location,
+                          temperature: currentWeather.temperature,
+                          minTemperature: currentWeather.minTemperature,
+                          maxTemperature: currentWeather.maxTemperature,
+                          icon: currentWeather.icon,
+                          weather: currentWeather.weather)
+    list.append(data)
+    list.append(contentsOf: forecast.compactMap {
+      return WidgetData(date: $0.forecasetedDate,
+                        location: location,
+                        temperature: $0.temperature,
+                        minTemperature: $0.minTemperature,
+                        maxTemperature: $0.maxTemperature,
+                        icon: $0.icon,
+                        weather: $0.weather)
+    })
+    
+    do {
+      let encoder = JSONEncoder()
+      let data = try encoder.encode(list)
+      try data.write(to: dataURL)
+    } catch let error {
+      print(error.localizedDescription)
+    }
+  }
+  
+  static func read() -> [WidgetData] {
+    guard let dataURL = dataURL,
+          let data = try? Data(contentsOf: dataURL) else {
+      return []
+    }
+    
+    let decoder = JSONDecoder()
+    do {
+      return try decoder.decode([WidgetData].self, from: data)
+    } catch let error {
+      print(error.localizedDescription)
+      return []
+    }
+  }
 }
