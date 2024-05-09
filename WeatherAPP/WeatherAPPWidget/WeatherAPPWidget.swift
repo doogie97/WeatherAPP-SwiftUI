1136f8c0542c42cd2fa6067b7c9f31169544a719
Doogie <chris_97@naver.com>
2024-05-09 11:16:53 +0900

    feat: 17버전 이후 프리뷰 대응

--- WeatherAPP/WeatherAPPWidget/WeatherAPPWidget.swift
+++ WeatherAPP/WeatherAPPWidget/WeatherAPPWidget.swift
@@ -9,23 +9,32 @@ import WidgetKit
 import SwiftUI
 
 struct WeatherAPPWidget: Widget {
-    let kind: String = "WeatherAPPWidget"
-    
-    var body: some WidgetConfiguration {
-        StaticConfiguration(kind: kind, provider: Provider()) { entry in
-            WeatherEntryView(entry: entry)
-                .padding()
-                .background()
-        }
-        .configurationDisplayName("날씨 위젯")
-        .description("현재 날씨를 확인할 위젯을 선택해 주세요.")
-        .supportedFamilies([.systemSmall])
+  let kind: String = "WeatherAPPWidget"
+  
+  var body: some WidgetConfiguration {
+    StaticConfiguration(kind: kind, provider: Provider()) { entry in
+      if #available(iOS 17.0, *) {
+          WeatherEntryView(entry: entry)
+              .containerBackground(for: .widget) {
+                  Color.clear
+              }
+              .padding()
+              .background()
+      } else {
+          WeatherEntryView(entry: entry)
+          .padding()
+          .background()
+      }
     }
+    .configurationDisplayName("날씨 위젯")
+    .description("현재 날씨를 확인할 위젯을 선택해 주세요.")
+    .supportedFamilies([.systemSmall])
+  }
 }
 
-struct WeatherAPPWidget_Previews: PreviewProvider {
-    static var previews: some View {
-        WeatherEntryView(entry: Provider.Entry(data: .preview))
-            .previewContext(WidgetPreviewContext(family: .systemSmall))
-    }
+@available(iOS 17.0, *)
+#Preview("KxWidgetWidget", as: .systemSmall) {
+  WeatherAPPWidget()
+} timeline: {
+  WeatherEntry(data: .preview)
 }
