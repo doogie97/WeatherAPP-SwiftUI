29fb7a06c5cacbad8c5c8c18991e896614fe9df5
Doogie <chris_97@naver.com>
2024-05-09 11:23:38 +0900

    feat: 위젯 화면 구성

--- WeatherAPP/WeatherAPPWidget/View/WeatherEntryView.swift
+++ WeatherAPP/WeatherAPPWidget/View/WeatherEntryView.swift
@@ -12,8 +12,41 @@ struct WeatherEntryView: View {
     var entry: Provider.Entry
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      if #available(iOS 17.0, *) {
+        contentsView
+        .containerBackground(for: .widget) {
+          Color.clear
+        }
+      } else {
+        contentsView
+      }
     }
+  
+  private var contentsView: some View {
+    ZStack {
+      LinearGradient(colors: [.orange, .blue],
+                     startPoint: .topLeading,
+                     endPoint: .bottomTrailing)
+      
+      VStack(alignment: .leading) {
+        Text(entry.data.location)
+        Text(entry.data.temperature)
+          .font(.largeTitle)
+        
+        Spacer()
+        
+        Image(systemName: entry.data.icon)
+          .symbolRenderingMode(.multicolor)
+        Text(entry.data.weather)
+          .font(.caption)
+        Text("최저 : \(entry.data.minTemperature) 최고 : \(entry.data.maxTemperature)")
+          .font(.caption)
+          .frame(maxWidth: .infinity, alignment: .leading)
+      }
+      .foregroundStyle(.white)
+      .padding()
+    }
+  }
 }
 
 struct WeatherEntryView_Previews: PreviewProvider {
